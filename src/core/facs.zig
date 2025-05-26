3d0bf4910e91ff428e4c35880a10ea0cb2a4337b
GustavoJCL <gjcalizayal@unjbg.edu.pe>
2025-05-26 07:20:00 -0500

    add: partial implementation of facts.zig

---
+++ src/core/facs.zig
@@ -0,0 +1,149 @@
+const std = @import("std");
+const logic = @import("logic.zig");
+const Node = logic.Node;
+const LogicNode = logic.LogicNode;
+const Allocator = std.mem.Allocator;
+
+pub const FactError = error{
+    TautologyDetected,
+    InconsistentImplications,
+    InconsistentAssumptions,
+    NotAnAndCondition,
+    InvalidRuleFormat,
+    UnknownOperator,
+    OutOfMemory,
+    LogicParsingFailed,
+};
+
+pub const LogicPair = struct {
+    atom: *Node,
+    value: bool,
+
+    pub const Context = struct {
+        pub fn hash(_: Context, key: LogicPair) u64 {
+            var hasher = std.hash.Wyhash.init(0);
+            key.atom.deepHashNodes(&hasher);
+            hasher.update(@as(u64, @intFromBool(key.value)));
+            return hasher.final();
+        }
+        pub fn eql(_: Context, a: LogicPair, b: LogicPair) bool {
+            return a.atom.eqlNodes(b.atom) and a.value == b.value;
+        }
+    };
+
+    pub fn deinit(_: LogicPair, allocator: Allocator) void {
+        _ = allocator;
+    }
+
+    pub fn format(self: LogicPair, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype, allocator: Allocator) !void {
+        const atom_str = try self.atom.toString(allocator);
+        defer allocator.free(atom_str);
+
+        if (self.value) {
+            try writer.print("({s}: True)", .{atom_str});
+        } else {
+            try writer.print("({s}: False)", .{atom_str});
+        }
+    }
+};
+
+pub fn baseFact(atom: *Node) *Node {
+    if (atom.logic_node == .Not) {
+        return atom.logic_node.Not.acquire();
+    } else {
+        return atom.acquire();
+    }
+}
+
+pub fn asPair(_: Allocator, atom: *Node) !LogicPair {
+    if (atom.logic_node == .Not) {
+        return .{ .atom = atom.logic_node.Not, .value = false };
+    } else {
+        return .{ .atom = atom, .value = false };
+    }
+}
+
+pub const Implication = struct {
+    from: *Node,
+    to: *Node,
+    pub const Context = struct {
+        pub fn hash(_: Context, key: Implication) u64 {
+            var hasher = std.hash.Wyhash.init(0);
+            key.from.deepHashNodes(&hasher);
+            key.to.deepHashNodes(&hasher);
+            return hasher.final();
+        }
+        pub fn eql(_: Context, a: Implication, b: Implication) bool {
+            return a.from.eqlNodes(b.from) and a.to.eqlNodes(b.to);
+        }
+    };
+
+    pub fn deinit(self: Implication, allocator: Allocator) void {
+        self.from.release(allocator);
+        self.to.release(allocator);
+    }
+};
+
+pub fn transitiveClosure(allocator: Allocator, initial_implications: std.ArrayList(Implication)) !std.HashMap(Implication, void, Implication.Context, std.hash_map.default_max_load_percentage) {
+    var full_implications = std.HashMap(Implication, void, Implication.Context, std.hash_map.default_max_load_percentage).init(allocator);
+    errdefer full_implications.deinit();
+
+    var literals = std.HashMap(*Node, void, Node.NodeContext, std.hash_map.default_max_load_percentage).init(allocator);
+    errdefer literals.deinit();
+
+    for (initial_implications.items) |impl| {
+        try full_implications.put(.{ .to = impl.to.acquire(), .from = impl.from.acquire() }, {});
+        try literals.put(impl.from.acquire(), {});
+        try literals.put(impl.to.acquire(), {});
+    }
+
+    var lit_k_it = literals.iterator();
+
+    while (lit_k_it.next()) |k_entry| {
+        const k = k_entry.key_ptr.*;
+        var liter_i_it = literals.iterator();
+        while (liter_i_it.next()) |i_entry| {
+            const i = i_entry.key_ptr.*;
+
+            if (full_implications.contains(.{ .from = i, .to = k })) {
+                var liter_j_it = literals.iterator();
+                while (liter_j_it.next()) |j_entry| {
+                    const j = j_entry.key_ptr.*;
+
+                    if (full_implications.contains(.{ .from = k, .to = j })) {
+                        try full_implications.put(.{ .from = i.acquire(), .to = j.acquire() }, {});
+                    }
+                }
+            }
+        }
+    }
+
+    var liter_cleanup_it = literals.iterator();
+    while (liter_cleanup_it.next()) |entry| {
+        entry.key_ptr.*.release(allocator);
+    }
+
+    return full_implications;
+}
+
+pub fn deduceAlphaImplication(allocator: Allocator, implication_list: std.ArrayList(Implication)) !std.HashMap(*Node, std.HashMap(*Node, void, Node.NodeContext, std.hash_map.default_max_load_percentage), Node.NodeContext, std.hash_map.default_max_load_percentage) {
+    var combined_implications = std.ArrayList(Implication).init(allocator);
+    defer {
+        for (combined_implications.items) |impl| impl.deinit(allocator);
+        combined_implications.deinit();
+    }
+
+    for (implication_list.items) |impl|
+        try combined_implications.append(.{ .from = impl.from.acquire(), .to = impl.to.acquire() });
+
+    for (implication_list.items) |impl| {
+        const not_to = try logic.createNot(allocator, impl.to);
+        defer not_to.release(allocator);
+        const not_from = try logic.createNot(allocator, impl.from);
+        defer not_from.release(allocator);
+
+        try combined_implications.append(.{ .from = not_to, .to = not_from });
+    }
+
+
+}
